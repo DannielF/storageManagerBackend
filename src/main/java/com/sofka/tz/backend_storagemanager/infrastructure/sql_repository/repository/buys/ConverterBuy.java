00bb76edae343205790948dad2df97cfdad56ae7
daniel.granados <daniel.granados@sofka.com.co>
2023-03-06 14:30:09 -0500

    Buy converter

--- src/main/java/com/sofka/tz/backend_storagemanager/infrastructure/sql_repository/repository/buys/ConverterBuy.java
+++ src/main/java/com/sofka/tz/backend_storagemanager/infrastructure/sql_repository/repository/buys/ConverterBuy.java
@@ -1,5 +1,7 @@
 package com.sofka.tz.backend_storagemanager.infrastructure.sql_repository.repository.buys;
 
+import com.sofka.tz.backend_storagemanager.domain.buy.entities.Buy;
+import com.sofka.tz.backend_storagemanager.infrastructure.sql_repository.repository.buys.data.BuyData;
 import org.springframework.stereotype.Component;
 /**
  * Buy Converter
@@ -11,4 +13,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConverterBuy {
 
+    public Buy toEntityBuy(BuyData buyData) {
+        Buy buy = new Buy();
+        buy.setId(buyData.getId());
+        buy.setIdType(buyData.getIdType());
+        buy.setIdNumber(buyData.getIdNumber());
+        buy.setClientName(buyData.getClientName());
+        buy.setProducts(buyData.getProducts());
+        return buy;
+    }
+
+    public BuyData toBuyData(Buy buy) {
+        BuyData buyData = new BuyData();
+        buyData.setId(buy.getId());
+        buyData.setIdType(buy.getIdType());
+        buyData.setIdNumber(buy.getIdNumber());
+        buyData.setClientName(buy.getClientName());
+        buyData.setProducts(buy.getProducts());
+        return buyData;
+    }
 }
